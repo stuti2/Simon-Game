72d2f805d3628ecdddaead30ae18f2c57a59f3c0
Stuti Singh <stuti1102.r@gmail.com>
2021-07-06 01:43:50 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,88 @@
+
+var userClickedPattern = [];
+
+var gamePattern = [];
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var started = false;
+
+var level=0;
+
+$(document).keypress(function(){
+  if(!started)
+  {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started=true;
+  }
+});
+
+$(".btn").click(function(){
+
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  //console.log(userClickedPattern);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length-1);
+});
+
+function checkAnswer(currentLevel){
+  if(userClickedPattern[currentLevel] === gamePattern[currentLevel])
+  {
+    console.log("success");
+    if(userClickedPattern.length === gamePattern.length)
+    {
+      setTimeout(function(){
+        nextSequence();
+      },1000);
+    }
+  }
+  else {
+    console.log("wrong");
+    var audio1 = new Audio("sounds/wrong.mp3");
+    audio1.play();
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    $("body").addClass("game-over");
+    setTimeout(function(){
+      $("body").removeClass("game-over");
+    },200);
+    startOver();
+  }
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
+
+
+function nextSequence(){
+  userClickedPattern = [];
+  level++;
+
+  $("#level-title").text("Level " + level);
+
+  var randomNumber = Math.floor(Math.random()*4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+
+}
+
+function animatePress(currentColour){
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function(){
+    $("#" + currentColour).removeClass("pressed");
+  },100);
+}
+
+function playSound(name){
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
